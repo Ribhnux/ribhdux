f0494ccc61f2f0317c0697fd6bf458b61b571f31
Ribhararnus Pracutiar <ribhararnus.pracutiar@gmail.com>
2018-09-03 23:48:37 +0700

    Fix(actionDispatcher), Add(selector)

--- src/decorators.ts
+++ src/decorators.ts
@@ -10,55 +10,130 @@ function createActionCreatorName(
   return `@${properName}`;
 }
 
+function $args(func) {
+  return (func + "")
+    .replace(/[/][/].*$/gm, "") // strip single-line comments
+    .replace(/\s+/g, "") // strip white space
+    .replace(/[/][*][^/*]*[*][/]/g, "") // strip multi-line comments
+    .split("){", 1)[0]
+    .replace(/^[^(]*[(]/, "") // extract the parameters
+    .replace(/=[^,]+/g, "") // strip any ES6 defaults
+    .split(",")
+    .filter(Boolean); // split & filter [""]
+}
+
+export function inputSelector(
+  target: any,
+  propertyKey: string | symbol,
+  parameterIndex: number
+) {
+  console.log("inputSelector", propertyKey, parameterIndex);
+}
+
 export function selector(
   target: any,
   key: string,
-  descriptor: TypedPropertyDescriptor<Ribhdux>
-) {
-  return descriptor;
-}
-
-export function actionDispatcher(
-  target: any,
-  key: string,
-  descriptor: PropertyDescriptor | undefined
+  descriptor: PropertyDescriptor
 ) {
   if (descriptor !== undefined) {
     const originalMethod = descriptor.value;
+    const targetName = target.constructor.name;
+
     descriptor.value = function(...args: any[]) {
       try {
-        const targetName = target.constructor.name;
-        const actionName = createActionCreatorName(targetName, key);
+        const nameArgs = $args(originalMethod);
+        const callFromOtherSelector =
+          this["callFrom"] === "createSelectorFromMethod";
+        const createSelectorFromMethodArgs: any[] = [
+          key,
+          callFromOtherSelector,
+          originalMethod,
+          ...nameArgs
+        ];
 
-        target.startDispatch.call(this, actionName);
-        originalMethod.apply(this, args);
-        target.commitDispatch.call(this);
+        const selector = target.createSelectorFromMethod.apply(
+          this,
+          createSelectorFromMethodArgs
+        );
+
+        if (callFromOtherSelector) {
+          delete this["callFrom"];
+        }
+
+        return selector;
       } catch (e) {
         console.error(e);
       }
     };
-
     return descriptor;
   }
 }
 
-export function asyncActionDispatcher(
-  target: any,
-  key: string,
-  descriptor: PropertyDescriptor | undefined
-) {
-  if (descriptor !== undefined) {
-    const originalMethod = descriptor.value;
+interface IActionDispatcherParams {
+  descriptor: PropertyDescriptor;
+  target: any;
+  originalMethod: any;
+  actionName: string;
+}
 
-    descriptor.value = async function(...args: any[]) {
-      const targetName = target.constructor.name;
-      const actionName = createActionCreatorName(targetName, key);
+function syncActionDispatcherDescriptor({
+  descriptor,
+  target,
+  originalMethod,
+  actionName
+}: IActionDispatcherParams) {
+  descriptor.value = function(...args: any[]) {
+    try {
+      target.startDispatch.call(this, actionName);
+      originalMethod.apply(this, args);
+      target.commitDispatch.call(this);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  return descriptor;
+}
 
+function asyncActionDispatcherDescriptor({
+  descriptor,
+  target,
+  originalMethod,
+  actionName
+}: IActionDispatcherParams) {
+  descriptor.value = async function(...args: any[]) {
+    try {
       target.startDispatch.call(this, actionName);
       await originalMethod.apply(this, args);
       target.commitDispatch.call(this);
-    };
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  return descriptor;
+}
 
-    return descriptor;
-  }
+export function actionDispatcher(isAsync = false) {
+  return function(
+    target: any,
+    methodName: string,
+    descriptor: PropertyDescriptor
+  ) {
+    if (descriptor !== undefined) {
+      const originalMethod = descriptor.value;
+      const targetName = target.constructor.name;
+      const actionName = createActionCreatorName(targetName, methodName);
+      const actionDispatcherDescriptorParams = {
+        descriptor,
+        target,
+        originalMethod,
+        actionName
+      };
+
+      const newDescriptor = isAsync
+        ? asyncActionDispatcherDescriptor(actionDispatcherDescriptorParams)
+        : syncActionDispatcherDescriptor(actionDispatcherDescriptorParams);
+
+      return newDescriptor;
+    }
+  };
 }
